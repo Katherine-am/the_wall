8c50330f83f4cf218ea2557d3c234368620c436b
Katherine Martin <katie.a.martin.18@gmail.com>
2019-09-22 17:26:35 -0700

    setting up main files

--- apps/wall_app/models.py
+++ apps/wall_app/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 class User(models.Model):
     pass
+
+class Message(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+    
--- apps/wall_app/views.py
+++ apps/wall_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .models import User
+from .models import User, Message, Comment
 
 def index(request):
     return render(request, 'index.html')
